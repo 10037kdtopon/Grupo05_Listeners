80e342a5a326636d316ce5da14b66d6558327118
Kevin David Topon Salazar <kdtopon@espe.edu.ec>
2024-01-22 15:25:00 -0500

    comit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import ClickExample from './components/ClickExample';
+import InputExample from './components/InputExample';
+import MouseOverExample from './components/MouseOverExample';
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>Eventos y Listeners en React</h1>
+        <h2>Grupo 05</h2>
+ 
+
+        {/* Componente ClickExample */}
+        <ClickExample />
+
+        {/* Componente InputExample */}
+        <InputExample />
+
+        {/* Nuevo componente MouseOverExample */}
+        <MouseOverExample />
+      </div>
+
+    </>
+  )
+}
+
+export default App
